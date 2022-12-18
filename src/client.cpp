82922b53d093125b91049e1aabfdbafcf3373821
Dan-Tan <dtan7891@gmail.com>
2022-12-18 23:47:53 +0800

    added get request

--- src/client.cpp
+++ src/client.cpp
@@ -4,25 +4,31 @@
 
 using namespace client 
 
-RiotApiClient::RiotApiClient(std::string keuy) {
-    api_key = key
-    easy_handle = curl_easy_init();
+RiotApiClient::RiotApiClient(std::string api_key) {
     curl_global_init(CURL_GLOBAL_ALL);
+    header = curl_slist_append(header, "X-RIOT-TOKEN: " + api_key);
+    easy_handle = curl_easy_init();
 }
 
 RiotApiClient::~RiotApiClient() {
     curl_global_cleanup();
 }
 
-std::string get_BASE_URL(std::string region) {
+std::string RiotApiClient::get_BASE_URL(std::string region) {
     return BASE_URL_START + region + BASE_URL_END;
 }
 
-std::string RiotApiClient::get(std::string end_url) {
+std::string RiotApiClient::get(std::string end_url, std::string region) {
+
+    std::string = this.get_BASE_URL(region)
 
     curl_easy_setopt(easy_handle, CURLOPT_URL, address.c_str());
-    curl_easy_setopt(easy_handle, CURLOPT_XOAUTH2_BEARER, api_key);
-    curl_easy_setopt(_connection, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
+    curl_easy_setopt(easy_handle, CURLOPT_HTTPGET, 1);
+    curl_easy_setopt(easy_handle, CURLOPT_HTTPHEADER, header);
+
+    res_ = curl_easy_perform();
+
+    return res_
 }
 
 
--- src/client.h
+++ src/client.h
@@ -9,11 +9,11 @@ using namespace client {
             RiotApiClient(std::string key);
             ~RiotApiCLient();
             
-            std::string get(std::string end_url);
+            std::string get(std::string end_url, std::string region);
             std::string get_BASE_URL(std::string region);
         private:
-            
-            std::string api_key;
+
+            struct curl_slist *header = NULL;
             std::string BASE_URL_START = "http://";
             std::string BASE_URL_END = ".api.riotgames.com";
             CURL* easy_handle;
