b249082d55ae99baf0b1598b3aa3e7ef058290a7
Dan-Tan <dtan7891@gmail.com>
2022-12-18 21:40:11 +0800

    Added Basic Client Structure

--- src/client.cpp
+++ src/client.cpp
@@ -1,6 +1,28 @@
-class Client {
-    public:
+#include <string.h>
+#include <jsoncpp/json/json.h>
+#include "client.h"
 
-    private:
-        
+using namespace client 
+
+RiotApiClient::RiotApiClient(std::string keuy) {
+    api_key = key
+    easy_handle = curl_easy_init();
+    curl_global_init(CURL_GLOBAL_ALL);
 }
+
+RiotApiClient::~RiotApiClient() {
+    curl_global_cleanup();
+}
+
+std::string get_BASE_URL(std::string region) {
+    return BASE_URL_START + region + BASE_URL_END;
+}
+
+std::string RiotApiClient::get(std::string end_url) {
+
+    curl_easy_setopt(easy_handle, CURLOPT_URL, address.c_str());
+    curl_easy_setopt(easy_handle, CURLOPT_XOAUTH2_BEARER, api_key);
+    curl_easy_setopt(_connection, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
+}
+
+
---
+++ src/client.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <curl/curl.h>
+#include <string>
+
+using namespace client {
+    class RiotApiClient {
+        public:
+            RiotApiClient(std::string key);
+            ~RiotApiCLient();
+            
+            std::string get(std::string end_url);
+            std::string get_BASE_URL(std::string region);
+        private:
+            
+            std::string api_key;
+            std::string BASE_URL_START = "http://";
+            std::string BASE_URL_END = ".api.riotgames.com";
+            CURL* easy_handle;
+
+    }
+}
